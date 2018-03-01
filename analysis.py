8ddc6e9a606a40e7be2a53f80af1b2de571723a0
nedikh <neda.khosravy@gmail.com>
2018-03-01 10:30:30 -0800

    be explicit in var name

---
+++ analysis.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 09:57:46 2018
+
+@author: nedakhosravi
+"""
+
+import pandas as pd
+oceania=pd.read_csv('/Users/nedakhosravi/Documents/DCW/data/gapminder_gdp_oceania.csv')
