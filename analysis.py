38de72fc18be4c2e2c4ea851507f483f06485c45
nedikh <neda.khosravy@gmail.com>
2018-03-01 11:21:58 -0800

    add comments

--- analysis.py
+++ analysis.py
@@ -5,6 +5,6 @@ Created on Thu Mar  1 09:57:46 2018
 
 @author: nedakhosravi
 """
-
+#oceania data
 import pandas as pd
 data_oceania=pd.read_csv('/Users/nedakhosravi/Documents/DCW/data/gapminder_gdp_oceania.csv',index_col='country')
